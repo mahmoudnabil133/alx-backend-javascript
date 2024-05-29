c8fa2667088295cc68632ed287a5241706bbdb88
mahmoudnabil133 <hodanabil144@gmail.com>
2024-05-29 11:25:05 +0300

    add element

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,6 @@
 export default function createEmployeesObject(departmentName, employees) {
   const myObj = {
-    departmentName: employees,
+    [departmentName]: employees,
   };
   return myObj;
 }
