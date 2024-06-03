768d63229cef5e7cc9870389a5726ab32da2f743
mahmoudnabil133 <hodanabil144@gmail.com>
2024-06-03 12:20:16 +0300

    merged grade data to studensts data

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,8 @@
+export default function updateStudentGradeByCity(students, city, grades) {
+  if (!(students instanceof Array)) return [];
+  return students.filter(st=> st.location === city).map((st)=>{
+    const stGrade = grades.filter(g=> g.studentId === st.id);
+    if (stGrade.length > 0) return { ...st, grade: stGrade[0].grade};
+    return {...st, grade:'N/A'}
+  })
+}
