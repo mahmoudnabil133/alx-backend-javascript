63b834530c9026c14cdba9e992844e88c0fd90dc
mahmoudnabil133 <hodanabil144@gmail.com>
2024-06-05 06:38:40 +0300

    set

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,16 +1,5 @@
 export default function hasValuesFromArray(set, arr) {
-  if (!(set instanceof Set)) {
-    throw new TypeError('set should be instance of Set');
-  }
-  if (!(arr instanceof Array)) {
-    throw new TypeError('arr should beinstance of Array');
-  }
-  for (el of arr) {
-    if (!(set.has(el))){
-      return false
-    }
-  }
-  return true 
+  return arr.every(val=> set.has(val))
 }
 // console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1]));
 // console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [10]));
