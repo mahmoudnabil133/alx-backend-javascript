9ed30396246e61e5cd56edcb589d95429165719b
mahmoudnabil133 <hodanabil144@gmail.com>
2024-06-05 06:49:18 +0300

    map

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,14 @@
+const groceriesList = ()=>{
+  const map = new Map()
+  
+    map.set('Apples', 10)
+    map.set('Tomatoes', 10)
+    map.set('Pasta', 1)
+    map.set('Rice', 1)
+    map.set('Banana', 5)
+
+    return map
+}
+
+console.log(groceriesList())
+// export default groceriesList
