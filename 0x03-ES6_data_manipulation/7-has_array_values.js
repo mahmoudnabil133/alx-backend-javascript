aea75bda5852b7b92a64bd564f851530dc7d42b7
mahmoudnabil133 <hodanabil144@gmail.com>
2024-06-05 06:41:22 +0300

    try

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,6 +1,3 @@
-export default function hasValuesFromArray(set, arr) {
-  return arr.every(val=> set.has(val))
-}
-// console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1]));
-// console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [10]));
-// console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1, 10]));
+const hasValuesFromArray = (set, array) => array.every((value) => set.has(value));
+
+export default hasValuesFromArray;
