2976aaa0537acecd788c9bc77008f5f904ca914a
mahmoudnabil133 <hodanabil144@gmail.com>
2024-08-19 23:26:57 +0300

    read csv file

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+const countStudents = (path) => {
+  const csvData = fs.readFileSync(path, 'utf8');
+  const lines = csvData.split('\n');
+  const Count = lines.length - 1;
+  const fields = { CS: [], SWE: [] };
+  for (let i = 1; i < lines.length; i += 1) {
+    const st = lines[i].split(',');
+    fields[st[3]].push(st[0]);
+  }
+  console.log(`Number of students: ${Count}`);
+  for (const k in fields) {
+    if (Object.prototype.hasOwnProperty.call(fields, k)) { // This is the safer way to check
+      const names = fields[k];
+      if (names.length > 0) {
+        console.log(`Number of students in ${k}: ${names.length}. List: ${names.join(', ')}`);
+      }
+    }
+  }
+};
+
+module.exports = countStudents;
