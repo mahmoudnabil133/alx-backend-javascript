95152180d2980fb4fa03772108abf0e9bd8a73ac
mahmoudnabil133 <hodanabil144@gmail.com>
2024-08-19 22:43:18 +0300

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -8,5 +8,5 @@ process.stdin.on('readable', () => {
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write(`This important software is now closing\n`);
+  process.stdout.write('This important software is now closing\n');
 });
