2e9e331acec4b2265af32e76bff98f0cc61d992b
mahmoudnabil133 <hodanabil144@gmail.com>
2024-08-20 11:27:12 +0300

    display message

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,15 +1,4 @@
-/**
- * Logs a message to the console.
- * @param {string} message - The message to log.
- * @returns {void}
- */
-// Arrow function
-const displayMessage = (message) => console.log(message);
-
-/**
- * Exports the displayMessage function.
- * @module displayMessage
- * @function
- */
-// Default module export.
+const displayMessage = (msg) => {
+    console.log(msg);
+}
 module.exports = displayMessage;
