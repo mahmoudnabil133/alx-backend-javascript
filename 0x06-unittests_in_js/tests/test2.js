ed114b83d50e9d1a3dbc0cc04e38edd7cf75025d
mahmoudnabil133 <hodanabil144@gmail.com>
2024-08-25 23:45:10 +0300

    imports updated

--- 0x06-unittests_in_js/tests/test2.js
+++
@@ -1,9 +0,0 @@
-// test mul function
-const assert = require('assert');
-describe('Array', function () {
-    describe('#mul()', function () {
-        it('should return 4 when 2*2', function () {
-        assert.equal(2 * 2, 4);
-        });
-    });
-    });
